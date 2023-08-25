7bc96b7e4374315b2170535ae194f6cb2749fefc
ihamzahkgithub <hamzahamid2192000@gmail.com>
2023-08-25 12:38:35 +0500

    dart files created

---
+++ lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+//import 'package:velocity_x/velocity_x.dart';
+
+const bgColor = Colors.black;
+const dividerColor = Colors.white;
+const textColor = Colors.white24;
+const cardColor = Colors.blueAccent;
